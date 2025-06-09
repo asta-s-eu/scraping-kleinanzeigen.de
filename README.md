5c7659597501ba27a66926774107d1ce0b3ef4f1
Andrei Danciuc <andrei.danciuc.ada+github@gmail.com>
2025-06-09 21:28:51 +0000

    release version 0.0.3

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# scraping-kleinanzeigen.de
+asta-s.eu.scraping.kleinanzeigen.de
+==========================
+
+Install
+-------
+
+```
+python -m venv /path/to/venv
+source /path/to/venv/bin/activate
+make update_pip_and_wheel
+make install_dev # or pip install . # for prod  
+```
+
+Verify and Validate
+-------------------
+
+```
+make code_check
+```
+
+Docker Images
+-------------
+
+Available at adan/asta-s-eu-kleinanzeigen-de
+
+
---
+++ src/asta_s_eu/scraping/kleinanzeigen_de/the_config.py
@@ -0,0 +1,41 @@
+"""
+Initiate constants by reading content from files
+"""
+import json
+import os
+from pathlib import Path
+
+from ciur import bnf_parser
+from ciur.rule import Rule
+
+PARSED_PAGES_LIMIT: int = 200
+
+_CONFIG_DIR = Path(
+    os.getenv('KLEINANZEIGEN_DE_CONFIG_FOLDER')
+    or
+    (Path(__file__).parent / '../../../../tests/KLEINANZEIGEN_DE_CONFIG_FOLDER')
+)
+
+FOLLOW_PERSONS: dict[str, str] = json.loads(
+    _CONFIG_DIR.joinpath("follow_persons.json").read_text()
+)
+IGNORE_PERSONS_BY_PRO_HREF: dict[str, str] = json.loads(
+    _CONFIG_DIR.joinpath("IGNORE_PERSONS_BY_PRO_HREF.json").read_text()
+)
+
+SEARCH: dict[str, str] = json.loads(
+    _CONFIG_DIR.joinpath("search.json").read_text()
+)
+
+_SEARCH_RULE = _CONFIG_DIR.joinpath("search_rule.ciur").read_text()
+
+_RES_SEARCH = bnf_parser.external2dict(_SEARCH_RULE)
+CIUR_SEARCH_RULE = Rule.from_list(_RES_SEARCH)
+
+__all__ = (
+    'CIUR_SEARCH_RULE',
+    'FOLLOW_PERSONS',
+    'IGNORE_PERSONS_BY_PRO_HREF',
+    'PARSED_PAGES_LIMIT',
+    'SEARCH',
+)
