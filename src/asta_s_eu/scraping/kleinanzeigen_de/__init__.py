b40f4d4a9217a8c2e6106dde4f3a0ef4dc264496
Andrei Danciuc <andrei.danciuc.ada+github@gmail.com>
2025-06-18 21:08:42 +0000

    change env GMail into EMail

--- src/asta_s_eu/scraping/kleinanzeigen_de/__init__.py
+++ src/asta_s_eu/scraping/kleinanzeigen_de/__init__.py
@@ -8,11 +8,11 @@ from asta_s_eu.scraping.core.log import get_loggers
 
 _, ALARM_LOG = get_loggers(Path(__file__), Path(__file__).parent / 'logging.yaml')
 
-GMAIL_USER = os.getenv("ADA_GMAIL_USER")
-assert GMAIL_USER
+EMAIL_FROM = os.getenv("ADA_EMAIL_FROM")
+assert EMAIL_FROM
 
-GMAIL_PASSWORD = os.getenv("ADA_GMAIL_PASSWORD")
-assert GMAIL_PASSWORD
+EMAIL_PASSWORD = os.getenv("ADA_EMAIL_PASSWORD")
+assert EMAIL_PASSWORD
 
-GMAIL_TO = os.getenv("ADA_GMAIL_TO") or ''
+EMAIL_TO = os.getenv("ADA_EMAIL_TO") or ''
 WEB_SITE = "kleinanzeigen.de"
